78f595c130349225bb15165a77adae69e303bc14
artem <bakirovartem69@gmail.com>
2023-01-09 17:15:56 +0100

    started with words game

---
+++ events/wordsGame.service.js
@@ -0,0 +1,111 @@
+
+//packages
+import axios from "axios";
+
+// models
+import WordsGameModel from "../models/wordsGameModel.js";
+
+//services
+import EmbedService from "./Embed.service.js";
+
+class WordsGameService{
+
+    static dictKey = process.env.YA_DISTIONARY_KEY;
+
+    static async getWordGame(message){
+        const wordsGame = await WordsGameModel.findOne({serverId: message.guild.id});
+        return wordsGame;
+    }
+
+    static async setWordsChanel(message) {
+        try{
+            const wordsGame = await WordsGameModel.findOne({serverId: message.guild.id});
+            if(wordsGame){
+                message.reply(`already set game channel. ChannelId is ${wordsGame.channelId}`);
+            } else{
+                const wordsGame = new WordsGameModel({
+                    serverId: message.guild.id,
+                    channelId: message.channel.id,
+                });
+                await wordsGame.save();
+                await EmbedService.createEmbed(null, "words", {chanel: message.channel});
+            }
+        }catch(e){
+            console.log("error", e);
+            message.reply("some error happened", e);
+        }
+
+    }
+
+    static async saveWord(message){
+        const word = message.content;
+        try{
+            const wordsGame = await WordsGameModel.findOne({serverId: message.guild.id});
+            await WordsGameModel.findOneAndUpdate({serverId: message.guild.id}, {usedWords: [word, ...wordsGame.usedWords]});
+        } catch(e){
+            console.log("error", e);
+            message.reply("some error happened", e);
+        }
+    }
+
+    static async checkWord(message) {
+        const word = message.content;
+        const result = await axios.get(`https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=${this.dictKey}&lang=ru-ru&text=${word}`)
+        console.log("api result", result.data, result.data.def[0]?.tr);
+        if(!result.data.def[0]?.text){
+            message.reply(`нет такого слова в этой букве! (ваше слово:${word})`);
+            setTimeout(async () => {
+                await message.delete();
+            }, 2000);
+            return;
+        }
+        const wordsGame = await WordsGameModel.findOne({serverId: message.guild.id});
+        const usedWords = wordsGame.usedWords;
+        const lastword = usedWords[0];
+        console.log("last word", lastword); //
+        if(lastword.at(-1) === word[0]) {
+            if(usedWords.indexOf(word) !== -1){
+                await message.react("❌");
+                await message.reply(`this word has already been used!`);
+                setTimeout(async () => {
+                    await message.delete();
+                }, 2000);
+                return;
+            }
+            await message.react("✅");
+            await this.saveWord(message);
+        } else {
+            await message.react("❌");
+            await message.reply(`wrong word! Your word must begin with ${lastword.at(-1)}`);
+            setTimeout(async () => {
+                await message.delete();
+            }, 2000);
+        }
+    }
+
+    static async activate(message){
+        const wordsGame = await WordsGameModel.findOne({serverId: message.guild.id});
+        await WordsGameModel.findOneAndUpdate({serverId: message.guild.id}, {gameActive: true});
+    }
+
+    static async tellResult() {
+
+    }
+
+    static async editEmbed() {
+
+    }
+
+    static async stopGame(message) {
+        const wordsGame = await WordsGameModel.findOne({serverId: message.guild.id});
+        if(!wordsGame){
+            await message.reply("there is no game. Start the game first with '$wordsgame' ")
+        } else{
+            await WordsGameModel.findOneAndDelete({serverId: message.guild.id});
+            await message.reply("game has been terminated. Thank you for playing");
+        }
+    }
+
+}
+
+export default WordsGameService;
--- index.js
+++ index.js
@@ -12,6 +12,11 @@ import LilithService from "./events/Lilith.service.js";
 import ManageChanelsService from "./events/ManageChanels.service.js";
 import EmbedService from "./events/Embed.service.js";
 import ManageServerService from "./events/ManageServer.service.js";
+import WordsGameService from "./events/wordsGame.service.js";
+import wordsGameService from "./events/wordsGame.service.js";
+
+// import models
+import WordsGameModel from "./models/wordsGameModel.js";
 
 export const client = new Discord.Client({intents: [
         GatewayIntentBits.Guilds,
@@ -19,6 +24,21 @@ export const client = new Discord.Client({intents: [
         GatewayIntentBits.MessageContent,
         GatewayIntentBits.GuildVoiceStates,
     ]});
+
+await client.login(process.env.BOT_TOKEN);
+
+client.on("ready", async () => {
+    console.log("bot ready!");
+    try{
+        client.user.setActivity("У меня есть апельсиновая пушка, и я иногда из неё стреляю");
+        await mongoose.connect(process.env.MONGO_URI, () => {
+            console.log("db connected");
+        });
+    } catch(e){
+        console.error("error", e);
+    }
+    console.log("I am ready for you, darling");
+});
 
 
 const prefix = "$";
@@ -28,9 +48,52 @@ client.on("voiceStateUpdate",(oldVoiceState,newVoiceState)=>{
 });
 
 client.on( "messageCreate", async (message) => {
-    await LilithService.react(message);
+    if (message.author.bot) return;
+
+
+    //***
+    // words game
+    const WordsGames = await WordsGameModel.find();
+    const WordsGameChannels = WordsGames.map((WordsGame) => {
+        return WordsGame.channelId;
+    })
+    console.log("ids", WordsGameChannels, message.channel.id);
+    if(WordsGameChannels.indexOf(message.channel.id) !== -1){
+            const wordGame = await wordsGameService.getWordGame(message);
+
+            if(message.content === "$wordsgamestop"){
+                await WordsGameService.stopGame(message);
+            }
+
+            if(message.mentions){
+                const mentions = message.mentions.users;
+                console.log("mentions", mentions);
+                mentions.forEach((mention) => {
+                    console.log("mentioned user id", mention.id);
+                }); // тут потом добавлять пользователей в базу данных
+                return;
+            }
+
+            if(!wordGame.gameActive) {
+                console.log("game move... analyze game move...");
+                if(message.content.startsWith(prefix)){
+                        await message.reply("wrong word. word must begin with regular letter of the alphabet");
+                        return;
+                }
+                await wordsGameService.activate(message);
+                await message.reply(`first word - ${message.content}. next word must begin with ${message.content[message.content.length - 1]}`);
+                await WordsGameService.saveWord(message);
+            } else {
+                await WordsGameService.checkWord(message)
+            }
+    }
+    // words game
+    // ***
+
+
+    //await LilithService.react(message); - тут иногда происходит ошибка.
     //console.log("message create", message);
-    console.log("content", message.content);
+    //console.log("content", message.content);
     if (message.author.bot) return;
     if (!message.content.startsWith(prefix)) return;
     const commandBody = message.content.slice(prefix.length);
@@ -94,9 +157,13 @@ client.on( "messageCreate", async (message) => {
             await ManageServerService.setWelcome(message, args);
             break;
         case "simjoin":
-            if (!args.length) return;
-            await ManageServerService.setWelcome(message, args);
-            break
+            client.emit("guildMemberAdd", message.member);
+            break;
+            //some games
+        case "wordsgame":
+            await WordsGameService.setWordsChanel(message);
+            break;
+
     }
 
     if(command === "greetme"){
@@ -104,25 +171,12 @@ client.on( "messageCreate", async (message) => {
     };
 });
 
-client.on("ready", async () => {
-    console.log("bot ready!");
-    client.emit("guildMemberAdd");
-    try{
-        client.user.setActivity("♂️ stick♂️  in ♂️ my ass ♂");
-        //client.user.setActivity("Hello, I am Lilith ;) ");
-        await mongoose.connect(process.env.MONGO_URI, () => {
-            console.log("db connected");
-        });
-    } catch(e){
-        console.error("error", e);
-    }
-    console.log("I am ready for you, darling");
-})
 
-await client.login(process.env.BOT_TOKEN);
+
+
 
 // если один юзер постоянно заходит и выходит, ивент не работает каждый раз, выходит.
-client.on("guildMemberAdd", ( )=> {
-    console.log("new user joined");
-    //await UserEventsService.onJoin(member)
+client.on("guildMemberAdd", async (member) => {
+    console.log("new user joined", member);
+    await UserEventsService.onJoin(member)
 });
