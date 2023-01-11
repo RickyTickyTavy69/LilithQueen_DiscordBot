5231eef25a18bb10caa4cd2c1962afde347f92c1
Bakirov Artem <bakirovartem69@gmail.com>
2023-01-11 14:36:45 +0100

    updated again infoService

--- events/getInfo.service.js
+++ events/getInfo.service.js
@@ -5,7 +5,7 @@ class GetInfoService{
     
     static async getFilmInfo(message, args){
         console.log("args[0]", args[0]);
-        const response = await axios.get(`https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=${args[0]}`, {
+        const response = await axios.get(`https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=${args.join(" ")}`, {
             headers: {
                 'X-API-KEY': 'c67e2162-ba5d-4237-8641-73178f8a9c29',
                 'Content-Type': 'application/json', 
