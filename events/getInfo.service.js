d1b8c0e4a4a56825ef4fd9c27fba00933d1bf749
Bakirov Artem <bakirovartem69@gmail.com>
2023-01-11 14:25:03 +0100

    updated infoService

--- events/getInfo.service.js
+++ events/getInfo.service.js
@@ -1,15 +1,22 @@
 import axios from "axios";
+import Discord, {EmbedBuilder} from "discord.js";
 
 class GetInfoService{
     
     static async getFilmInfo(message, args){
-        const response = await axios.get("https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword/bunraku", {
+        console.log("args[0]", args[0]);
+        const response = await axios.get("https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=bunraku", {
             headers: {
                 'X-API-KEY': 'c67e2162-ba5d-4237-8641-73178f8a9c29',
                 'Content-Type': 'application/json', 
             }
         })
-        message.reply(`response: ${response.data}`);
+        const filmEmbed = new EmbedBuilder()
+        .setTitle(`${response.data.films[0].nameRu}`)
+        .setColor(0xCC397B)
+        .setDescription(`${response.data.films[0].description}`)
+        .setImage(`${response.data.films[0].posterUrl}`);
+        await message.channel.send({embeds: [filmEmbed]});
         console.log(response.data); 
     }
 
