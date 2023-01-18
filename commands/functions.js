ab4748bc79e87fb140f6af6cfccc0e4d6f2d0cf2
artem <bakirovartem69@gmail.com>
2023-01-18 17:37:46 +0100

    fixed buttons like dislike

--- commands/functions.js
+++ commands/functions.js
@@ -146,13 +146,45 @@ export default [
         },
         async execute(interaction, targetUser) {
             const message = interaction.message;
-            const user = targetUser
+            const user = targetUser.username;
             const userId = targetUser.id;
             const author = interaction.user.username;
             const authorId = interaction.user.id;
             const guild = interaction.guild;
             const member = guild.members.cache.get(userId);
             const like = +interaction.customId.split(" ")[2] + 1;
+            const dislike = +interaction.customId.split(" ")[3]
+            const row = new ActionRowBuilder()
+                .addComponents(
+                    new ButtonBuilder()
+                        .setCustomId(`like ${userId} ${like} ${dislike}`)
+                        .setLabel(`Like (${like} ❤)`)
+                        .setStyle(ButtonStyle.Success),
+                )
+                .addComponents(
+                    new ButtonBuilder()
+                        .setCustomId(`dislike ${userId} ${like} ${dislike}`)
+                        .setLabel(`Dislike (${dislike} 💀)`)
+                        .setStyle(ButtonStyle.Danger),
+                )
+            const embed = await EmbedService.createEmbed("ava", {user, author,  data: {member}})
+            console.log("embed is...", embed);
+            message.edit({embeds: [embed], components: [row]});
+        }
+    },
+    {
+        data: {
+            name: "dislike"
+        },
+        async execute(interaction, targetUser) {
+            const message = interaction.message;
+            const user = targetUser.username;
+            const userId = targetUser.id;
+            const author = interaction.user.username
+            const authorId = interaction.user.id;
+            const guild = interaction.guild;
+            const member = guild.members.cache.get(userId);
+            const like = +interaction.customId.split(" ")[2];
             const dislike = +interaction.customId.split(" ")[3] + 1;
             const row = new ActionRowBuilder()
                 .addComponents(
@@ -164,39 +196,7 @@ export default [
                 .addComponents(
                     new ButtonBuilder()
                         .setCustomId(`dislike ${userId} ${like} ${dislike}`)
-                        .setLabel(`Like (${dislike} 💀)`)
-                        .setStyle(ButtonStyle.Danger),
-                )
-            const embed = await EmbedService.createEmbed("ava", {user, author,  data: {member}})
-            console.log("embed is...", embed);
-            message.edit({embeds: [embed], components: [row]});
-        }
-    },
-    {
-        data: {
-            name: "Dislike"
-        },
-        async execute(interaction, targetUser) {
-            const message = interaction.message;
-            const user = targetUser
-            const userId = targetUser.id;
-            const author = interaction.user.username;
-            const authorId = interaction.user.id;
-            const guild = interaction.guild;
-            const member = guild.members.cache.get(userId);
-            const like = +interaction.customId.split(" ")[2] + 1;
-            const dislike = +interaction.customId.split(" ")[3] + 1;
-            const row = new ActionRowBuilder()
-                .addComponents(
-                    new ButtonBuilder()
-                        .setCustomId(`like ${userId} ${like}`)
-                        .setLabel(`Like (${like} ❤)`)
-                        .setStyle(ButtonStyle.Success),
-                )
-                .addComponents(
-                    new ButtonBuilder()
-                        .setCustomId(`dislike ${userId} ${dislike}`)
-                        .setLabel(`Like (${dislike} 💀)`)
+                        .setLabel(`Dislike (${dislike} 💀)`)
                         .setStyle(ButtonStyle.Danger),
                 );
             const embed = await EmbedService.createEmbed("ava", {user, author,  data: {member}})
