3b7221b4c852949a4b00f53fe915794b016d399e
artem <bakirovartem69@gmail.com>
2023-02-09 15:19:36 +0100

    data edited weather request

--- api/weatherRequestMethods.js
+++ api/weatherRequestMethods.js
@@ -4,9 +4,9 @@ class WeatherRequestMethods{
     static async getLocation(name, interaction){
         try{
             const result = await axios.get(`http://api.positionstack.com/v1/forward?access_key=d97b984a15c1a4153a5ca201565fbce4&query=moscow`);
-            console.log("result is...", result);
-            const lat = result.data[0].latitude.toFixed(4);
-            const long = result.data[0].longitude.toFixed(4);
+            console.log("result is...", result.data);
+            const lat = result.data.data[0].latitude.toFixed(4);
+            const long = result.data.data[0].longitude.toFixed(4);
             return {lat, long};
         } catch(e){
             interaction.reply({content: "an error while looking up the location. Please enter another location.", ephemeral: true});
