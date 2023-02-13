a52ca9a2fe67c7ce7615c097d40cebe8c684f691
artem <bakirovartem69@gmail.com>
2023-02-13 08:08:31 +0100

    update token

--- api/wordsgameMethods.js
+++ api/wordsgameMethods.js
@@ -9,7 +9,7 @@ class WordsgameMethods{
         try{
             const result = await axios.get(`https://developers.lingvolive.com/api/v1/Translation?text=${word}&srcLang=1049&dstLang=1033&isCaseSensitive=false`, {
                 headers: {
-                    Authorization: `Bearer ZXlKaGJHY2lPaUpJVXpJMU5pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SmxlSEFpT2pFMk56WXlPVEEzTWpFc0lrMXZaR1ZzSWpwN0lrTm9ZWEpoWTNSbGNuTlFaWEpFWVhraU9qVXdNREF3TENKVmMyVnlTV1FpT2pjek9ESXNJbFZ1YVhGMVpVbGtJam9pT1Rrd04yVXhOamN0TnpaaU1DMDBNV016TFdFelptUXRObVV3WWpJd1pEa3dOelZrSW4xOS54d2lub29TM3oxa2RldzFhdjZIalM0OGx0WEhvaFFrLUF5a1Bab2VUM1g0`
+                    Authorization: `Bearer ZXlKaGJHY2lPaUpJVXpJMU5pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SmxlSEFpT2pFMk56WXpOVGc1TlRBc0lrMXZaR1ZzSWpwN0lrTm9ZWEpoWTNSbGNuTlFaWEpFWVhraU9qVXdNREF3TENKVmMyVnlTV1FpT2pjek9ESXNJbFZ1YVhGMVpVbGtJam9pT1Rrd04yVXhOamN0TnpaaU1DMDBNV016TFdFelptUXRObVV3WWpJd1pEa3dOelZrSW4xOS5nY1p6elEzQTlpZlh2UWFhaE5OSXd5Mzh4WGdWMVVobVk3STRCVHEyTG1B`
                 }
             });
             message.react("✅");
